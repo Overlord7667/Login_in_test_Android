2c208ee8fa94de567d0100b696951a79d038ae08
Overlord7667 <aspiaro@gmail.com>
2023-09-30 10:43:05 +0300

    add button

--- app/src/main/java/com/betelgeuse/corp/login_in_test_android/MainActivity.java
+++ app/src/main/java/com/betelgeuse/corp/login_in_test_android/MainActivity.java
@@ -2,7 +2,10 @@ package com.betelgeuse.corp.login_in_test_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
+import android.graphics.PaintFlagsDrawFilter;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView textView = findViewById(R.id.textLog);
+        TextView textView2 = findViewById(R.id.textPas);
+
+
+        Paint paint = textView.getPaint();
+        paint.setFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+        Paint paint1 = textView2.getPaint();
+        paint1.setFlags(Paint.UNDERLINE_TEXT_FLAG);
     }
 }
